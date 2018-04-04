cd1ff4b41b96356f205af105fac9a4ab9e98a09f
manoj.surelia <manoj18091@gmail.com>
2018-04-05 00:35:08 +0530

    Boot quick start

---
+++ SpringBootQuickStart/src/main/java/com/spring/bootquick/BootApiApp.java
@@ -0,0 +1,13 @@
+package com.spring.bootquick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApiApp.class, args);
+	}
+
+}
